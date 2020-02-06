0958fd0b6f956185843177da87c0d26657ff362e
Jacob Gold <jkobygold@gmail.com>
2020-02-06 16:11:10 -0500

    Fixed in with javascript. There is a weird bug though that needs to be fixed

--- triviaGame/assets/javascript/app.js
+++ triviaGame/assets/javascript/app.js
@@ -1,37 +1,73 @@
 
 
-
 $(document).ready(function() {
-
+   
     $('#quiz').hide();
-    $('#Results').hide();
-
-var correct = 0;
-var incorrect = 0;
-var time =80;
+    $('.Results').hide();
+    $('#timer').hide();
 
 
-$("#start").on("click", function() {
-    $('#start').hide();
-    $('#quiz').show();
-  });
+        var correct = 0;
+        var incorrect = 0;
+        var incomplete = 0;
+        var elapse;
+        var secs = 120;
+
+
+           function clock() {
+            time = setInterval(oneTwo, 1000);
+        
+            function oneTwo() {
+                if (secs === 0) {
+                    clearInterval(time);
+                    $('#quiz').hide();
+                    $('.Results').show();
+                    $('#timer').hide();
+                }
+                if (secs > 0) {
+                    secs--;   
+                }
+                $("#time").html(secs);
+            }
+        }
+
+ $("#start").on("click", function() {
+            $('#start').hide();
+            $('#timer').show();
+            $('#quiz').show();
+            clock();
+            $("#time").html(secs);
+          });
+
+
 
  
 
+        $('input[type=radio]').on ('change', function(){
+            correct = $('input[value=correct]:checked').length;
+            incorrect = $('input[value=wrong]:checked').length;
+            incomplete = $(6-(correct+incorrect));
+            
+            });
 
 
-$("#finished").on("click", function() {
-   
-    // replace radio questions with the results
 
-      });
+          $("#submit").on("click", function() {
+            $('.Results').show();
+            $('#quiz').hide();
+            $('#timer').hide();
+            $('#start').hide();
+            
+            $("#correct").html(correct);
+            $("#incorrect").html(incorrect);
+            $("#empty").html(empty);
 
-
-  
-$("#finished").on("click", function() {
-   
-    // replace radio questions with the results
+        });
         
-        });    
+            
+    
+ 
+
+
     });
 
