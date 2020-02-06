a6dd618043816dff0f792052c91c5d18c906fbe4
Jacob Gold <jkobygold@gmail.com>
2020-02-06 12:34:51 -0500

    started html form

---
+++ triviaGame/assets/javascript/app.js
@@ -0,0 +1,37 @@
+
+
+
+$(document).ready(function() {
+
+    $('#quiz').hide();
+    $('#Results').hide();
+
+var correct = 0;
+var incorrect = 0;
+var time =80;
+
+
+$("#start").on("click", function() {
+    $('#start').hide();
+    $('#quiz').show();
+  });
+
+ 
+
+
+
+$("#finished").on("click", function() {
+   
+    // replace radio questions with the results
+
+      });
+
+
+  
+$("#finished").on("click", function() {
+   
+    // replace radio questions with the results
+        
+        });    
+    });
+
